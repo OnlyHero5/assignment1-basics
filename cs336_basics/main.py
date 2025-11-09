71339e3be86e7861144abcaf6b5318bb7306e05b
PSX <1440455308@qq.com>
2025-11-09 19:45:20 +0800

    debug

--- cs336_basics/main.py
+++ cs336_basics/main.py
@@ -1,3 +1,5 @@
 from train_bpe import BPE
 
-BPE.train_bpe(input_path="../data/TinyStoriesV2-GPT4-train.txt", vocab_size=10000, speical_tokens=["<|endoftext|>"])
+BPE.train_bpe(input_path="../data/TinyStoriesV2-GPT4-train.txt", 
+              vocab_size=10000, 
+              special_tokens=["<|endoftext|>"])
