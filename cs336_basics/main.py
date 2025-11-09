8bbbe7b6a0aaf51556894d960ef0289118722ecf
PSX <1440455308@qq.com>
2025-11-09 20:34:29 +0800

    test

--- cs336_basics/main.py
+++ cs336_basics/main.py
@@ -1,5 +1,5 @@
 from train_bpe import BPE
 
-BPE.train_bpe(input_path="../data/", 
+BPE.train_bpe(input_path="../data/owt_train.txt", 
               vocab_size=32000, 
               special_tokens=["<|endoftext|>"])
