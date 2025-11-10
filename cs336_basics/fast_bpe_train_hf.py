25fdd015cfa83b409365f1dade32402545f5bce0
PSX <1440455308@qq.com>
2025-11-10 19:20:28 +0800

    测试框架速度

---
+++ cs336_basics/fast_bpe_train_hf.py
@@ -0,0 +1,31 @@
+from tokenizers import Tokenizer, models, trainers, pre_tokenizers, decoders
+from tokenizers.processors import TemplateProcessing
+from pathlib import Path
+from __future__ import annotations
+
+
+
+
+def train_bpelevel_bpe(
+    input_path: str | Path,
+    vocab_size: int,
+    special_tokens: list[str] | None = None,
+    min_frequency: int = 2,
+    output_path: str | Path  = "../data",
+) :
+    
+    save_dir = Path(output_path)
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    tokenizer = Tokenizer(models.BPE(unk_token=None))
+    tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=False)
+    tokenizer.decoder = decoders.ByteLevel()
+
+    trainer = trainers.BpeTrainer(
+        vocab_size=vocab_size,
+        min_frequency=min_frequency,
+        special_tokens=special_tokens,
+        show_progress=True,
+    )
+    tokenizer.train([str(input_path)], trainer=trainer)
+    tokenizer.model.save(str(save_dir / "tokenizer.json"))
--- cs336_basics/main.py
+++ cs336_basics/main.py
@@ -1,5 +1,11 @@
-from train_bpe import BPE
+# from train_bpe import BPE
 
-BPE.train_bpe(input_path="../data/TinyStoriesV2-GPT4-train.txt", 
-              vocab_size=10000, 
-              special_tokens=["<|endoftext|>"])
+# BPE.train_bpe(input_path="../data/TinyStoriesV2-GPT4-train.txt", 
+#               vocab_size=10000, 
+#               special_tokens=["<|endoftext|>"])
+
+from fast_bpe_train_hf import train_bpelevel_bpe
+
+train_bpelevel_bpe(input_path="../data/TinyStoriesV2-GPT4-train.txt",
+                   vocab_size=10000,
+                   special_tokens=["<|endoftext|>"])
