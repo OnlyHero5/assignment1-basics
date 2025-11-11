623dca17e44a287bb60fe47f351e37056f024589
PSX <1440455308@qq.com>
2025-11-11 19:55:19 +0800

    OWT_BPE训练

--- cs336_basics/main.py
+++ cs336_basics/main.py
@@ -6,7 +6,7 @@
 
 from fast_bpe_train_hf import train_bpelevel_bpe
 
-train_bpelevel_bpe(input_path="../data/TinyStoriesV2-GPT4-train.txt",
+train_bpelevel_bpe(input_path="../data/owt_train.txt",
                    vocab_size=10000,
                    special_tokens=["<|endoftext|>"],
-                   output_path="../data/TinyStoriesV2")
+                   output_path="../data/OWT")
